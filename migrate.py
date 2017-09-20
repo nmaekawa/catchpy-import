e08a8a541ec5a5444670f69621464739167098e9
nmaekawa <nmaekawa@g.harvard.edu>
2017-09-20 13:54:05 -0400

    fixes specific to course-v1:HarvardX+AmPoX.6+2T2016 migration:
    - request timeout raised to 5min
    - ignore ssl because i don't have a proper certificate
    - cleanup for errors found in this poetry set
    - fix in page size calculation
    - still dependent of catchpy requirements...
    - added a find_reply_to_reply command

--- migrate.py
+++ migrate.py
@@ -41,15 +41,20 @@ from anno.models import Anno
 from consumer.catchjwt import encode_catchjwt
 
 
-DEFAULT_REQUESTS_TIMEOUT = 5
+DEFAULT_REQUESTS_TIMEOUT = 300
 SEARCH_PAGE_SIZE = 500
 
 class CatchSearchClient(object):
+    """ perform searches in catch backend; _always_ in AnnotatorJS context."""
 
-    def __init__(self, base_url, api_key,
-                 secret_key, user=None, timeout=None):
+    def __init__(self,
+                 base_url,     # eg. "http://catch.harvardx.harvard.edu/catch/annotator/search"
+                 api_key,
+                 secret_key,
+                 user=None,
+                 timeout=None):
 
-        self.url = urljoin(base_url, '/catch/annotator/search')
+        self.url = base_url
         self.api_key = api_key
         self.secret_key = secret_key
         self.timeout = timeout or DEFAULT_REQUESTS_TIMEOUT
@@ -66,16 +71,16 @@ class CatchSearchClient(object):
             user=user, ttl=86400).decode('utf-8')
 
 
-    def fullset_size(self, contextId=None):
+    def fullset_size(self, context_id=None):
 
         # first search to check how many records to retrieve
         params = {
             'limit': 1,
             'offset': 0,
-            'contextId': contextId,
+            'contextId': context_id,
         }
         resp = requests.get(
-            self.url, params=params,
+            self.url, params=params, verify=False,
             headers=self.default_headers, timeout=self.timeout)
         resp.raise_for_status()
 
@@ -83,16 +88,16 @@ class CatchSearchClient(object):
         return search_content['total']
 
 
-    def fetch_page(self, contextId=None, offset=0, limit=1):
+    def fetch_page(self, context_id=None, offset=0, limit=1):
 
         # first search to check how many records to retrieve
         params = {
             'limit': limit,
             'offset': offset,
-            'contextId': contextId,
+            'contextId': context_id,
         }
         resp = requests.get(
-            self.url, params=params,
+            self.url, params=params, verify=False,
             headers=self.default_headers, timeout=self.timeout)
         resp.raise_for_status()
 
@@ -146,135 +151,46 @@ def make_token(source_url, api_key, secret_key, user):
     click.echo(client.make_token_for_user(user))
 
 
-@click.command()
-@click.option('--outdir', default='tmp', help='output DIRECTORY; default=./tmp')
-@click.option('--offset_start', default=0,
-              help='if not pulling all set, specify offset to start')
-@click.option('--source_url', required=True, help='include http/https')
-@click.option('--api_key', required=True)
-@click.option('--secret_key', required=True)
-@click.option('--context_id', required=True)
-@click.option('--catcha_ok/--skip-catcha', default=True,
-              help='default is to convert to catcha, as pulled from source')
-@click.option('--reuse_outdir/--no_reuse',
-              'exist_ok', default=False, help='default=no_reuse')
-def pull_from_source(outdir, offset_start, source_url,
-                     api_key, secret_key, context_id,
-                     catcha_ok, exist_ok):
-
-    # create output dir
-    try:
-        os.makedirs(outdir, mode=0o776, exist_ok=exist_ok)
-    except OSError:
-        click.echo('ERROR: outdir already exists({})'.format(outdir))
-        return
-
-    # create searchClient
-    client = CatchSearchClient(
-        base_url=source_url, api_key=api_key,
-        secret_key=secret_key, user='admin')
-
-    # search all context_ids?
-    search_context_id = None if context_id == 'None' else context_id
-
-    # loop for fetching fullset
-    try:
-        total_len = client.fullset_size(contextId=search_context_id)
-    except Exception as e:
-        click.echo('ERROR: unable to fetch fullset_size: {}'.format(e))
-        return
-
-    total_pages = int(total_len/SEARCH_PAGE_SIZE)
-    if total_len % SEARCH_PAGE_SIZE > 0:
-        total_pages += 1
-
-    # save info about this search
-    fullset_name = clean_to_alphanum_only(context_id)
-    fullset_info = {
-        'source_url': source_url,
-        'api_key': api_key,
-        'context_id': context_id,
-        'total_rows': total_len,
-    }
-    save_to_file(outdir=outdir,
-                 filename='info_annojs_{0}.json'.format(fullset_name),
-                 json_content=fullset_info)
-    click.echo('total_len({}), total_pages({})'.format(total_len, total_pages))
-
-
-    # need to pull slices of result
-    page_no = 1
-    current_len = 0
-    offset = offset_start
-    while current_len < total_len:
-        try:
-            page_content = client.fetch_page(
-                contextId=search_context_id, offset=offset, limit=SEARCH_PAGE_SIZE)
-        except Exception as e:
-            click.echo('ERRO: {}'.format(e))
-            return
-        else:
-            save_to_file(outdir=outdir,
-                         filename='annojs_{0}_{1:06d}.json'.format(
-                             fullset_name, page_no),
-                         json_content=page_content)
-
-            if catcha_ok:
-                convert_and_save(json_content=page_content['rows'],
-                                 workdir=outdir,
-                                 filename='catcha_{0}_{1:06d}.json'.format(
-                                     fullset_name, page_no))
-
-            current_len += int(page_content['size'])
-            offset += int(page_content['size'])
-            click.echo('next page_no({}); current_len({})'.format(page_no,
-                                                                  current_len))
-            page_no += 1
-
-
 
 @click.command()
 @click.option('--workdir', default='tmp', help='directory for input/output; default=./tmp')
-@click.option('--context_id', required=True)
-def convert(workdir, context_id):
-    fullset_name = clean_to_alphanum_only(context_id)
-    all_files = sorted(os.listdir(workdir))
-    filename_regex = \
-            r'^annojs_' + re.escape(fullset_name) + '_(?P<page_no>[0-9]{3})'
+@click.option('--filepath', required=True)
+def convert(workdir, filepath):
 
-    # loop through input files
-    for filename in all_files:
-        if fnmatch.fnmatch(filename, 'annojs_{}*'.format(fullset_name)):
-            # parse filename to get the page number
-            match = re.search(filename_regex, filename)
-            if match is None:  # skip file
-                continue
+    with open(filepath, 'r') as f:
+        annojs_content = json.load(f)
 
-            page_no = match.group('page_no')
+    # hammering fixes
+    annojs_ok = []
+    annojs_messed = []
+    for c in annojs_content:
+        if 'media' in c:
+            if len(c['ranges']) > 0:
+                if 'start' not in c['ranges'][0]:
+                    c['ranges'][0]['start'] = ""
+                    c['ranges'][0]['end'] = ""
+            annojs_ok.append(c)
+        else:
+            annojs_messed.append(c)
 
-            # read file contents
-            path = os.path.join(workdir, filename)
-            with open(path, 'r') as f:
-                annojs_content = json.load(f)
+    # get input filename as base for output filenames
+    basename = os.path.basename(filepath)
 
-            # convert and save
-            catcha_filename = 'catcha_{0}_{1}.json'.format(fullset_name, page_no)
-            convert_and_save(json_content=annojs_content['rows'],
-                             workdir=workdir,
-                             filename=catcha_filename)
+    # save messed up
+    save_to_file(workdir, 'messed_{}'.format(basename), annojs_messed)
 
-
-def import_to_db(catcha_list, resp_filepath):
-    jwt_payload = {'override': ['CAN_IMPORT']}
-
-    resp = CRUD.import_annos(catcha_list, jwt_payload)
+    # convert and save
+    catcha_filename = 'catcha_{}'.format(basename)
+    (catcha_list, error_list) =  convert_and_save(
+        json_content=annojs_ok,
+        workdir=workdir,
+        filename=catcha_filename)
 
 
 
 def convert_and_save(json_content, workdir, filename):
-    click.echo('workdir({}), filename({}), json_content({})'.format(workdir,
-                                                                    filename,
-                                                                    len(json_content)))
+    click.echo('workdir({}), filename({}), json_content({})'.format(
+        workdir, filename, len(json_content)))
 
     # convert
     (catcha_list, error_list) = convert_to_catcha(json_content)
@@ -290,83 +206,6 @@ def convert_and_save(json_content, workdir, filename):
                      json_content=error_list)
 
     return(catcha_list, error_list)
-
-
-@click.command()
-@click.option('--workdir', default='tmp', help='directory for input/output; default=./tmp')
-@click.option('--context_id', required=True)
-def push_to_target(workdir, context_id):
-    fullset_name = clean_to_alphanum_only(context_id)
-    all_files = sorted(os.listdir(workdir))
-    filename_regex = \
-            r'^catcha_' + re.escape(fullset_name) + '_(?P<page_no>[0-9]{3})'
-
-    # loop through input files
-    for filename in all_files:
-        if fnmatch.fnmatch(filename, 'catcha_{}*'.format(fullset_name)):
-            # parse filename to get the page number
-            match = re.search(filename_regex, filename)
-            if match is None:  # skip file
-                continue
-
-            page_no = match.group('page_no')
-
-            # read file contents
-            path = os.path.join(workdir, filename)
-
-            click.echo('pushing filename: {}'.format(filename))
-
-            with open(path, 'r') as f:
-                catcha_content = json.load(f)
-
-            # sort by anno_id to avoid importing reply before comment
-            ordered_catcha_list = sorted(
-                catcha_content, key=lambda k: k['id'])
-
-            save_to_file(workdir,
-                         filename='sorted_{}.json'.format(filename),
-                         json_content=ordered_catcha_list)
-
-            '''
-            resp_filename = 'error_{}'.format(filename)
-            resp_filepath = os.path.join(workdir, resp_filename)
-
-            import_to_db(ordered_catcha_list, resp_filepath)
-            '''
-
-@click.command()
-@click.option('--workdir', default='tmp', help='directory for input/output; default=./tmp')
-@click.option('--context_id', required=True)
-def debug(workdir, context_id):
-    fullset_name = clean_to_alphanum_only(context_id)
-    all_files = sorted(os.listdir(workdir))
-    filename_regex = \
-            r'^catcha_' + re.escape(fullset_name) + '_(?P<page_no>[0-9]{3})'
-
-    # loop through input files
-    for filename in all_files:
-        if fnmatch.fnmatch(filename, 'catcha_{}*'.format(fullset_name)):
-            # parse filename to get the page number
-            match = re.search(filename_regex, filename)
-            if match is None:  # skip file
-                continue
-
-            page_no = match.group('page_no')
-
-            # read file contents
-            path = os.path.join(workdir, filename)
-
-            click.echo('pushing filename: {}'.format(filename))
-
-            with open(path, 'r') as f:
-                catcha_content = json.load(f)
-
-            # sort by anno_id to avoid importing reply before comment
-            ordered_catcha_list = sorted(
-                catcha_content, key=lambda k: k['id'])
-
-            for c in ordered_catcha_list:
-                click.echo('id({}) created({})'.format(c['id'], c['created']))
 
 
 
@@ -401,7 +240,7 @@ def pull_all(outdir, offset_start, source_url,
 
     # loop for fetching fullset
     try:
-        total_len = client.fullset_size(contextId=search_context_id)
+        total_len = client.fullset_size(context_id=search_context_id)
     except Exception as e:
         click.echo('ERROR: unable to fetch fullset_size: {}'.format(e))
         return
@@ -432,24 +271,28 @@ def pull_all(outdir, offset_start, source_url,
     more_to_pull = True
     while more_to_pull and page_no < 50000:
         try:
+            click.echo('************** pulling page({}), offset({}), limit({})'.format(
+                page_no, offset, SEARCH_PAGE_SIZE))
             page_content = client.fetch_page(
-                contextId=search_context_id, offset=offset, limit=SEARCH_PAGE_SIZE)
+                context_id=search_context_id, offset=offset, limit=SEARCH_PAGE_SIZE)
         except Exception as e:
             click.echo('ERRO: {}'.format(e))
             return
         else:
+            size = 0
             for c in page_content['rows']:
                 if c['id'] in fullset_anno:
+                    # sanity check, not supposed to happen (i think)
                     click.echo('GAAAAAAAAAAAAAAAAAAAAAH, duplicate({})'.format(c['id']))
                 else:
                     fullset_anno[c['id']] = c
+                    size += 1
 
-            size = int(page_content['size'])
             current_len += size
             offset += size
             more_to_pull = size > 0
-            click.echo('next page_no({}); current_len({}); more?({})'.format(
-                page_no, current_len, more_to_pull))
+            click.echo('next page_no({}); this batch size({}); current_len({}); more?({})'.format(
+                page_no, size, current_len, more_to_pull))
             page_no += 1
 
     click.echo('FINISH pulling from source, total({})'.format(len(fullset_anno)))
@@ -458,10 +301,11 @@ def pull_all(outdir, offset_start, source_url,
                  filename='fullset_annojs_{}.json'.format(fullset_name),
                  json_content=list(fullset_anno.values()))
 
-    (catcha_list, error_list) = convert_and_save(
-        json_content=list(fullset_anno.values()),
-        workdir=outdir,
-        filename='fullset_catcha_{}.json'.format(fullset_name))
+
+def print_db_info():
+    db = getattr(settings, 'DATABASES')
+    click.echo('db_host({}) db_name({})'.format(
+        db['default']['HOST'], db['default']['NAME']))
 
 
 @click.command()
@@ -469,37 +313,31 @@ def pull_all(outdir, offset_start, source_url,
 @click.option('--filepath', required=True, help='filepath for input file')
 def push_from_file(workdir, filepath):
 
+    print_db_info()
+
     with open(filepath, 'r') as f:
         catcha_list = json.load(f)
 
-    # sort between comment and replies
-    comment_list = []
-    reply_list = []
-    for c in catcha_list:
-        if Catcha.is_reply(c):
-            reply_list.append(c)
-        else:
-            comment_list.append(c)
+    # order by id (prevent a reply before associated annotation)
+    ordered_catcha_list = sorted(
+        catcha_list, key=lambda k: k['id'])
 
-    click.echo('comments({}), replies({})'.format(len(comment_list),
-                                                  len(reply_list)))
-
+    # mock payload to match permissions
     jwt_payload = {'override': ['CAN_IMPORT']}
-    resp_comment = CRUD.import_annos(comment_list, jwt_payload)
-    resp_reply = CRUD.import_annos(reply_list, jwt_payload)
 
-    failed_list = resp_comment['failed'] + resp_reply['failed']
+    resp = CRUD.import_annos(ordered_catcha_list, jwt_payload)
+    failed_list = resp['failed']
     save_to_file(outdir=workdir,
                  filename='fail_to_push_from_file_{}'.format(
                      os.path.basename(filepath)),
                 json_content=failed_list)
 
 
-
 @click.command()
 @click.option('--workdir', default='tmp', help='output DIRECTORY; default=./tmp')
 @click.option('--context_id', required=True)
 def clear_anno_in_context_id(workdir, context_id):
+    print_db_info()
 
     anno_list = Anno._default_manager.filter(
         raw__platform__context_id=context_id)
@@ -518,17 +356,94 @@ def clear_anno_in_context_id(workdir, context_id):
                 json_content=failed_list)
 
 
+@click.option('--workdir', default='tmp', help='output DIRECTORY; default=./tmp')
+@click.command()
+def find_reply_to_reply(workdir):
+    print_db_info()
+
+    reply_list = Anno._default_manager.all().exclude(
+        anno_reply_to_id=None)
+
+    reply_to_reply_list = []
+    for r in reply_list:
+        for rr in r.replies:
+            reply_to_reply_list.append(rr.serialized)
+
+    save_to_file(outdir=workdir,
+                 filename='reply_to_reply.json',
+                 json_content=reply_to_reply_list)
+
+
+@click.command()
+@click.option('--workdir', default='tmp', help='output DIRECTORY; default=./tmp')
+@click.option('--input_filepath_1', required=True)
+@click.option('--input_filepath_2', required=True)
+def compare_annojs(workdir, input_filepath_1, input_filepath_2):
+    """works like file1 contains file2, so set the original set of annojs to
+    file1 and the imported set to file2
+    """
+
+    with open(input_filepath_1, 'r') as f:
+        annojs_list_1 = json.load(f)
+
+    with open(input_filepath_2, 'r') as f:
+        annojs_list_2 = json.load(f)
+
+    annojs2 = {x['id']: x for x in annojs_list_2}
+
+    click.echo('dict has ({}) keys'.format(len(annojs2.keys())))
+
+    results = {
+        'not_found': [],
+        'not_similar': [],
+        'passed': [],
+    }
+    compare = ''
+    for a in annojs_list_1:
+        if a['id'] in annojs2.keys():
+            b = annojs2[a['id']]
+            # will compare serialized json
+            for x in [a , b]:
+                x['uri'] = str(x['uri'])
+                # these props were dropped from legacy
+                for key in ['archived', 'citation', 'deleted']:
+                    try:
+                        del x[key]
+                    except KeyError:
+                        pass  # ok if these already don't exist
+
+                try:  # v2 omits if empty string
+                    if len(x['quote']) == 0:
+                        del x['quote']
+                except KeyError:
+                    pass  # ok if these already don't exist
+
+
+            if AnnoJS.are_similar(a, b):
+                results['passed'].append(a)
+            else:
+                results['not_similar'].append(a)
+                results['not_similar'].append(b)
+        else:
+            results['not_found'].append(a)
+
+    for category in ['not_found', 'not_similar', 'passed']:
+        save_to_file(outdir=workdir,
+                     filename='{}.json'.format(category),
+                     json_content=results[category])
+
+
+
 
 if __name__ == "__main__":
 
-    cli.add_command(pull_from_source)
     cli.add_command(pull_all)
     cli.add_command(convert)
-    cli.add_command(push_to_target)
     cli.add_command(push_from_file)
-    cli.add_command(debug)
     cli.add_command(clear_anno_in_context_id)
     cli.add_command(make_token)
+    cli.add_command(compare_annojs)
+    cli.add_command(find_reply_to_reply)
     cli()
 
 
